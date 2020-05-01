86d6b0d8b063caf3a60ade9800bbd73e501adcaa
inkinkz <nithiphorn.n@gmail.com>
2020-05-01 23:16:15 +0700

    added href

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,7 +3,7 @@ import React from "react";
 const NavBar = () => {
   return (
     <nav className="navbar navbar-light bg-dark">
-      <a className="navbar-brand text-white" href="#">
+      <a className="navbar-brand text-white" href="https://id-checker.now.sh/">
         ระบบเช็ครายชื่อผู้เดินทางกลับจาก จ.ภูเก็ต
       </a>
     </nav>
